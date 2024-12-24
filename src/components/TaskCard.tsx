9d26278312fe3c9eda67de5481905a5f1f071c32
MuhammedBasith <iambasith123@gmail.com>
2024-12-25 01:02:04 +0530

    complete logic to display trashicon when mouse enter on task card

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -22,9 +22,10 @@ function TaskCard({ task }: Props) {
             }}
             className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose500 cursor-grab relative">
             {task.content}
-            <button className="stroke-white absolute right-4 top-1/2-translate-y-1/2 bg-columnBackgroundColor p-2 rounded">
+
+            {mouseIsOver && <button className="stroke-white absolute right-4 top-1/2-translate-y-1/2 bg-columnBackgroundColor p-2 rounded">
                 <TrashIcon />
-            </button>
+            </button>}
         </div>
     )
 }
