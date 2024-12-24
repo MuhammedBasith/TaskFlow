e36dd7e43fb42d32b532724268bc6931ebe2818d
MuhammedBasith <iambasith123@gmail.com>
2024-12-25 01:00:51 +0530

    add logic to track mouse while over a task

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -13,7 +13,14 @@ function TaskCard({ task }: Props) {
 
 
     return (
-        <div className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose500 cursor-grab relative">
+        <div
+            onMouseEnter={() => {
+                setMouseIsOver(true)
+            }}
+            onMouseLeave={() => {
+                setMouseIsOver(false)
+            }}
+            className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose500 cursor-grab relative">
             {task.content}
             <button className="stroke-white absolute right-4 top-1/2-translate-y-1/2 bg-columnBackgroundColor p-2 rounded">
                 <TrashIcon />
